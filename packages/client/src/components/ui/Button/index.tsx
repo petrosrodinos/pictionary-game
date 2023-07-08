c7fb1fe95fb838910806e1f7fdaec4443204f1ec
Petros <petros.rodinos@yahoo.com>
2023-07-08 18:18:06 +0300

    button component

--- packages/client/src/components/ui/Button/index.tsx
+++ packages/client/src/components/ui/Button/index.tsx
@@ -1,7 +1,43 @@
 import { FC } from "react";
+import Typography from "../Typography";
+import { ImSpinner6 } from "react-icons/im";
+import "./style.scss";
 
-const Button: FC = () => {
-  return <div></div>;
+interface ButtonProps {
+  title: string;
+  variant?: "primary" | "secondary";
+  style?: any;
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
+  loading?: boolean;
+  icon?: any;
+}
+
+const Button: FC<ButtonProps> = ({
+  variant = "primary",
+  style,
+  onClick,
+  className,
+  title,
+  disabled,
+  loading,
+  icon,
+  ...props
+}) => {
+  const Icon = icon;
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`button-container button-${variant} ${className}`}
+      {...props}
+    >
+      {icon && !loading && <Icon className="button-icon" />}
+      {loading && <ImSpinner6 className="button-icon icon-loading" />}
+      <Typography>{title}</Typography>
+    </button>
+  );
 };
 
 export default Button;
