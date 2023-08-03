402b2928879fa9f16e5cc982ea86cd6186e02846
Eleftherios <121286401+Tsountanis@users.noreply.github.com>
2023-08-03 11:06:04 +0300

    recent games

--- packages/client/src/pages/home/RecentGames/index.tsx
+++ packages/client/src/pages/home/RecentGames/index.tsx
@@ -4,13 +4,11 @@ import RecentGamesItem from "./Item";
 import { getRandomAvatar } from "../../../utils/avatar";
 import "./style.scss";
 
-
 const currMonth = new Date().getMonth();
 const currYear = new Date().getFullYear();
 const currDay = new Date().getDate();
 const currHour = new Date().getHours();
 const currMin = new Date().getMinutes();
-
 
 const TestRecentGamesItems: UserType[] = [...new Array(20)].map((_, index) => {
   return {
@@ -18,20 +16,21 @@ const TestRecentGamesItems: UserType[] = [...new Array(20)].map((_, index) => {
     rank: 1 + index,
     avatar: getRandomAvatar(),
     username: `username${index}`,
-    xp: 50- index * 2,
+    xp: 50 - index * 2,
     games: 30 - index,
-    date: ` ${currDay-index}/${currMonth}/${currYear}, ${currHour}:${currMin-index}`,
+    date: ` ${currDay}/${currMonth}/${currYear}, ${currHour}:${
+      currMin - index
+    }`,
   };
 });
-
 
 const RecentGames: FC = () => {
   return (
     <div className="recent-leader-board-container">
       <div className="recent-games-label">
-      <Typography variant="sub-header-main">Recent Games</Typography>
+        <Typography variant="header-main">My Recent Games</Typography>
       </div>
-      <div className="recent-leader-board-content">
+      <div className="recent-games-content">
         {TestRecentGamesItems.map((item: any, index: number) => {
           return <RecentGamesItem key={index} item={item} />;
         })}
