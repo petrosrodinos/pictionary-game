5a9d62c35574c8d8c843e8f58bc89cbc6f8ffa4a
Petros <petros.rodinos@yahoo.com>
2023-08-19 14:12:54 +0300

    made ChipSelector to accept default value and name

--- packages/client/src/components/ui/ChipSelector/index.tsx
+++ packages/client/src/components/ui/ChipSelector/index.tsx
@@ -1,19 +1,28 @@
-import { FC, useState } from "react";
+import { FC, useState, useEffect } from "react";
 import Typography from "../Typography";
 import "./style.scss";
 
 interface ChipSelectorProps {
   chips: string[];
-  onChange: (value: string) => void;
+  name: string;
+  value?: string;
+  onChange: (data: { name: string; value: string }) => void;
   style?: React.CSSProperties;
 }
 
-const ChipSelector: FC<ChipSelectorProps> = ({ chips, onChange, style }) => {
+const ChipSelector: FC<ChipSelectorProps> = ({ chips, value, name, onChange, style }) => {
   const [selectedChip, setSelectedChip] = useState<string>("");
+
+  useEffect(() => {
+    setSelectedChip(value || "");
+  }, [value]);
 
   const handleChange = (value: string) => {
     setSelectedChip(value);
-    onChange(value);
+    onChange({
+      name,
+      value,
+    });
   };
   return (
     <div style={style} className="chip-selector-container">
