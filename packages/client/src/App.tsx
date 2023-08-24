fd46f6514003bfb7b598c77bc69ac7d1a4bd09e4
Petros <petros.rodinos@yahoo.com>
2023-08-24 20:53:22 +0300

    added protected and default routes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,21 +4,32 @@ import Login from "./pages/auth/login";
 import AuthPage from "./pages/auth";
 import Home from "./pages/home";
 import Room from "./pages/room";
+import { authStore } from "./store/authStore";
 import "./App.scss";
 
 function App() {
+  const { isLoggedIn } = authStore((state) => state);
   return (
     <BrowserRouter>
       <div className="main-container">
         <Routes>
-          <Route path="/" element={<Navigate to={`user/login`} />}></Route>
-          <Route path="/home" element={<Home />} />
-          <Route path="/room/:id" element={<Room />} />
-          <Route path="/user" element={<AuthLayout />}>
-            <Route path="register" element={<Register />} />
-            <Route path="login" element={<Login />} />
-          </Route>
-          <Route path="*" element={<Navigate to="" />} />
+          {isLoggedIn && (
+            <>
+              <Route
+                path="/"
+                element={<Navigate to={isLoggedIn ? "/home" : "/user/login"} />}
+              ></Route>
+              <Route path="/home" element={<Home />} />
+              <Route path="/room/:id" element={<Room />} />
+            </>
+          )}
+          {!isLoggedIn && (
+            <Route path="/user" element={<AuthLayout />}>
+              <Route path="register" element={<Register />} />
+              <Route path="login" element={<Login />} />
+            </Route>
+          )}
+          <Route path="*" element={<Navigate to={isLoggedIn ? "/home" : "/user/login"} />} />
         </Routes>
       </div>
     </BrowserRouter>
