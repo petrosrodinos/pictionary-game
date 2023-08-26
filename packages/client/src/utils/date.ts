69e389b1e9bb853e1c930da398b7a716cb367b0d
Petros <petros.rodinos@yahoo.com>
2023-08-26 20:49:10 +0300

    added time on recent games

--- packages/client/src/utils/date.ts
+++ packages/client/src/utils/date.ts
@@ -4,6 +4,8 @@ export function formatDate(inputDate: string) {
   const dd = String(date.getDate()).padStart(2, "0");
   const mm = String(date.getMonth() + 1).padStart(2, "0");
   const yyyy = date.getFullYear();
+  const hh = date.getHours();
+  const min = date.getMinutes();
 
-  return `${dd}/${mm}/${yyyy}`;
+  return `${dd}/${mm}/${yyyy}, ${hh}:${min}`;
 }
