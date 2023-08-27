fb0a6030c8cd3c6981f4b5a642b8860bc180c6c0
Petros <petros.rodinos@yahoo.com>
2023-08-27 20:51:55 +0300

    fix color

--- packages/client/src/constants/colors.ts
+++ packages/client/src/constants/colors.ts
@@ -1,10 +1,9 @@
-export const TEXT_SECONDARY = "#1c2129";
+export const TEXT_SECONDARY = "#611515";
 
 export const COLORS: any = {
   "--text-primary": "#1c2129",
   "--text-secondary": "#611515",
   "--text-secondary-light": "white",
-  // "--primary-background-color": "#e89253",
   "--primary-background-color": "rgba(232, 146, 83, 0.9)",
   "--secondary-background-color-dark": "#925151",
   "--secondary-background-color-medium": "#eac7c7",
