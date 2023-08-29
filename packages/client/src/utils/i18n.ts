6e0a3e5eb8b553e661a99671a2c278f8ef3d3ef6
Petros <petros.rodinos@yahoo.com>
2023-08-29 20:42:50 +0300

    fixed translation bug

--- packages/client/src/utils/i18n.ts
+++ packages/client/src/utils/i18n.ts
@@ -1,21 +1,28 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import English from "../locales/en/translation.json";
+import Greek from "../locales/gr/translation.json";
+
+const resources = {
+  en: {
+    translation: English,
+  },
+  gr: {
+    translation: Greek,
+  },
+};
 
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
-  .use(HttpApi)
   .init({
     supportedLngs: ["en", "gr"],
     fallbackLng: "en",
+    resources,
     detection: {
       order: ["localStorage", "cookie"],
       caches: ["localStorage"],
-    },
-    backend: {
-      loadPath: "../../public/locales/{{lng}}/translation.json",
     },
   });
 
