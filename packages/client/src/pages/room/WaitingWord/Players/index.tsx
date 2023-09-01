b72108ae167fe380000d8c4c2134d5993756fc03
Petros <petros.rodinos@yahoo.com>
2023-09-01 23:08:29 +0300

    added transition on Players rank

--- packages/client/src/pages/room/WaitingWord/Players/index.tsx
+++ packages/client/src/pages/room/WaitingWord/Players/index.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Avatar from "../../../../components/ui/Avatar";
 import Typography from "../../../../components/ui/Typography";
 import { UserType } from "../../../../interfaces/typing";
+import { useTransition, animated } from "react-spring";
 import "./style.scss";
 
 interface PlayersProps {
@@ -9,28 +10,37 @@ interface PlayersProps {
 }
 
 const Players: FC<PlayersProps> = ({ players }) => {
+  const sortedPlayers = [...players].sort((a, b) => b.points - a.points);
+
+  const transitions = useTransition(sortedPlayers, {
+    key: (player: UserType) => player.username,
+    from: { transform: "translateY(-20px)", opacity: 0 },
+    enter: { transform: "translateY(0px)", opacity: 1 },
+    leave: { transform: "translateY(-20px)", opacity: 0 },
+    trail: 50,
+    delay: 200,
+  });
+
   return (
     <>
       <Typography variant="text-accent" className="rank-label">
         RANK
       </Typography>
       <div className="players-container">
-        {players
-          .sort((a, b) => b.points - a.points)
-          ?.map((player, index) => (
-            <div key={index} className="player-container">
-              <div className="players-content">
-                <div className="player-rank">
-                  <Typography>{index + 1}</Typography>
-                </div>
-                <Avatar image={player.avatar} />
-                <div className="player-info">
-                  <Typography variant="text-main">@{player.username}</Typography>
-                  <Typography>points: {player.points}</Typography>
-                </div>
+        {transitions((style, player, _, index) => (
+          <animated.div key={index} className="player-container" style={style}>
+            <div className="players-content">
+              <div className="player-rank">
+                <Typography>{index + 1}</Typography>
+              </div>
+              <Avatar image={player.avatar} />
+              <div className="player-info">
+                <Typography variant="text-main">@{player.username}</Typography>
+                <Typography>points: {player.points}</Typography>
               </div>
             </div>
-          ))}
+          </animated.div>
+        ))}
       </div>
     </>
   );
