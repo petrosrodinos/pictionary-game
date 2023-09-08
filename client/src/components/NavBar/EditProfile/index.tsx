111d93975a4ef7bd586fd4a4c4ca8112a45bb24c
Petros <petros.rodinos@yahoo.com>
2023-09-08 21:31:10 +0300

    added spinner when getting profile

--- client/src/components/NavBar/EditProfile/index.tsx
+++ client/src/components/NavBar/EditProfile/index.tsx
@@ -4,16 +4,18 @@ import Register from "../../../pages/auth/register";
 import { getUser } from "../../../services/user";
 import { authStore } from "../../../store/authStore";
 import Preferences from "./Settings";
+import Spinner from "../../ui/Spinner";
 import "./style.scss";
 
 const EditProfile: FC = () => {
   const { userId } = authStore((state) => state);
 
-  const { data } = useQuery(["user", userId], () => getUser(userId));
+  const { data, isLoading } = useQuery(["user", userId], () => getUser(userId));
   return (
     <div className="edit-info">
       <Preferences />
-      <Register values={data} isEditing={true} />
+      <Spinner style={{ paddingTop: "5px" }} loading={isLoading} />
+      {!isLoading && <Register values={data} isEditing={true} />}
     </div>
   );
 };
