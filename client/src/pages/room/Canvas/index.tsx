05f2af1c3799e43b7493c8fe19a3f5215e457d6b
Petros <petros.rodinos@yahoo.com>
2023-09-19 14:15:03 +0300

    first try

--- client/src/pages/room/Canvas/index.tsx
+++ client/src/pages/room/Canvas/index.tsx
@@ -4,6 +4,10 @@ import DrawingOptions from "./DrawingOptions";
 import { useTranslation } from "react-i18next";
 import CanvasDraw from "react-canvas-draw";
 import { BrushSizes } from "../../../constants/game";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import { drawHand } from "./utilities";
+import "@tensorflow/tfjs-backend-webgl";
 import "./style.scss";
 
 interface CanvasProps {
@@ -14,31 +18,90 @@ interface CanvasProps {
 }
 
 const Canvas: FC<CanvasProps> = ({ word, currentUserIsPlaying, canvasData, socket }) => {
+  const webcamRef = useRef<any>(null);
+  const canvasRef = useRef<any>(null);
   const ref = useRef<any>(null);
   const { t } = useTranslation();
   const [color, setColor] = useState<string>("#000");
   const [lineWidth, setLineWidth] = useState<number>(BrushSizes[0]);
-  // const [saveData, setSaveData] = useState<any>(null);
-  // const [canvasWidth, setCanvasWidth] = useState<number>(700);
-  // const [canvasHeight, setCanvasHeight] = useState<number>(600);
+  const currentLineRef = useRef<{ brushColor: string; brushRadius: number; points: any[] }>({
+    brushColor: color,
+    brushRadius: lineWidth,
+    points: [],
+  });
+  const [isDrawing, setIsDrawing] = useState(false);
+  let drawTimeout: any = null;
 
-  // useEffect(() => {
-  //   const updateCanvasSize = () => {
-  //     const canvasElement = ref.current?.parentNode as HTMLElement;
-  //     if (canvasElement) {
-  //       const { width, height } = canvasElement.getBoundingClientRect();
-  //       setCanvasWidth(width);
-  //       setCanvasHeight(height);
-  //     }
-  //   };
+  useEffect(() => {
+    runHandpose();
+  }, []);
 
-  //   window.addEventListener("resize", updateCanvasSize);
-  //   updateCanvasSize();
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log("Handpose model loaded.");
+    setInterval(() => {
+      detect(net);
+    }, 50);
+  };
 
-  //   return () => {
-  //     window.removeEventListener("resize", updateCanvasSize);
-  //   };
-  // }, []);
+  const detect = async (net: any) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const hand = await net.estimateHands(video);
+      if (hand.length > 0) {
+        console.log("Hand detected");
+
+        const fingerTip = hand[0].annotations.indexFinger[3];
+        const relativeX = (fingerTip[0] / videoWidth) * 700;
+        const relativeY = (fingerTip[1] / videoHeight) * 600;
+
+        currentLineRef.current.points.push({ x: relativeX, y: relativeY });
+
+        setIsDrawing(true);
+
+        if (drawTimeout) clearTimeout(drawTimeout);
+
+        drawTimeout = setTimeout(() => {
+          commitDrawingToCanvas();
+        }, 2000);
+      }
+      // Draw mesh
+      // const ctx = canvasRef.current.getContext("2d");
+      // drawHand(hand, ctx);
+    }
+  };
+
+  const commitDrawingToCanvas = () => {
+    if (currentLineRef.current.points.length > 0) {
+      const existingData = ref.current
+        ? JSON.parse(ref.current.getSaveData())
+        : { width: 700, height: 600, lines: [] };
+      existingData.lines.push(currentLineRef.current);
+      ref.current?.loadSaveData(JSON.stringify(existingData));
+
+      // Clear currentLineRef
+      currentLineRef.current = {
+        brushColor: color,
+        brushRadius: lineWidth,
+        points: [],
+      };
+    }
+  };
+
+  useEffect(() => {
+    console.log("drawing", isDrawing);
+  }, [isDrawing]);
 
   useEffect(() => {
     ref.current?.clear();
@@ -85,8 +148,36 @@ const Canvas: FC<CanvasProps> = ({ word, currentUserIsPlaying, canvasData, socke
 
   return (
     <div className="canvas-panel-container">
-      {currentUserIsPlaying && (
+      {true && (
         <>
+          <Webcam
+            ref={webcamRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zIndex: 9,
+              width: 200,
+              height: 200,
+            }}
+          />
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zIndex: 9,
+              width: 200,
+              height: 200,
+            }}
+          />
           <div className="word-container">
             <Typography variant="text-accent" className="word-label">
               {t("word")}:
@@ -106,7 +197,7 @@ const Canvas: FC<CanvasProps> = ({ word, currentUserIsPlaying, canvasData, socke
           />
         </>
       )}
-      {/*fucking bug on the component so i am doing it that way */}
+
       <CanvasDraw
         ref={ref}
         hideGrid={true}
